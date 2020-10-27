46c3010b7afaa8b08cddacdcdb2687384f3e9f10
luca cavicchioli <luca.cavicchioli.88@gmail.com>
2020-10-27 17:31:28 +0100

    pari o dispari

---
+++ pari e dispari/js/script.js
@@ -0,0 +1,68 @@
+function lancioDadiPari() {
+  var numeroUser = Math.floor((Math.random() * 5) + 1);
+  var numeroPC = Math.floor((Math.random() * 5) + 1);
+  console.log(numeroUser);
+  console.log(numeroPC);
+
+  var somma = (numeroUser + numeroPC);
+  document.getElementById('somma').innerHTML = somma;
+  document.getElementById('numeroUser').innerHTML = numeroUser;
+  document.getElementById('numeroPc').innerHTML = numeroPc;
+
+
+
+  if (somma % 2 === 0){
+    document.getElementById('esitoUser').innerHTML = 'hai vinto';
+    document.getElementById('esitoPc').innerHTML = 'hai perso';
+  } else {
+    document.getElementById('esitoUser').innerHTML = 'hai perso';
+    document.getElementById('esitoPc').innerHTML = 'hai vinto';
+  }
+
+
+};
+
+function lancioDadiDispari() {
+  var numeroUser = Math.floor((Math.random() * 5) + 1);
+  var numeroPC = Math.floor((Math.random() * 5) + 1);
+  console.log(numeroUser);
+  console.log(numeroPC);
+  var somma = (numeroUser + numeroPC);
+  document.getElementById('somma').innerHTML = somma;
+  document.getElementById('numeroUser').innerHTML = numeroUser;
+  document.getElementById('numeroPc').innerHTML = numeroPc;
+
+
+  if (somma % 2 != 0){
+    document.getElementById('esitoUser').innerHTML = 'hai vinto';
+    document.getElementById('esitoPc').innerHTML = 'hai perso';
+  } else {
+    document.getElementById('esitoUser').innerHTML = 'hai perso';
+    document.getElementById('esitoPc').innerHTML = 'hai vinto';
+  }
+
+
+
+};
+
+
+
+//
+
+
+
+
+
+
+
+
+
+// function lancioDadi() {
+//   var numeroUser = Math.floor((Math.random() * 5) + 1);
+//   document.getElementById("esitoUser").innerHTML = numeroUser;
+// }
+//
+// function lancioDadi() {
+//   var numeroPC = Math.floor((Math.random() * 5) + 1);
+//   document.getElementById("esitoPc").innerHTML = numeroPC;
+// }
